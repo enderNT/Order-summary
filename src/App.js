4c48f2b9710e4371f8cdb92bc1c1f5e3179b67b8
enderNT12 <gayagocr@gmail.com>
2021-12-21 07:30:41 -0600

    terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import iconmusic from './images/icon-music.svg'
+import ilustrationhero from './images/illustration-hero.svg'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+      <div className='container'>
+        <img src={ilustrationhero} alt='ilustration-hero' className='IlustrationHero' />
+        <h1>Order Summary</h1>
+        <p className='Description'>
+          You can now listen to millions of songs,
+          audiobooks, and podcast on any device
+          anywhere you like!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='Info'>
+          <img src={iconmusic} alt='icon-music' className='IconMusic' />
+          <div className='Plan'>
+            <h4>Anual plan</h4>
+            <p className='Price'>$59.99/year</p>
+          </div>
+          <button className='Change'>Change</button>
+        </div>
+        <button className='PayOut'>Proceed to payment</button>
+        <button className='Cancel'>Cancel Order</button>
+      </div>
+
     </div>
   );
 }
